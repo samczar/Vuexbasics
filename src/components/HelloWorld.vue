d50da09245dcbb59b374847e710eb548d143bad8
AnyankaChibuzo <senetor.samczar@gmail.com>
2019-04-10 22:47:00 +0200

    Vuex Basics

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="left">
+      <h1>{{title}}</h1>
+      <form action @submit.prevent="addLink">
+        <input type="text" class="link-input" placeholder="Add a new link" v-model="newLink">
+      </form>
+      <ul>
+        <li v-for="(link, index) in links" v-bind:key="index">
+          {{link}}
+          <button v-on:click="removeLinks(index)" class="rm">Remove</button>
+        </li>
+      </ul>
+    </div>
+    <div class="right">
+      <stats/>
+    </div>
   </div>
 </template>
 
 <script>
+import Stats from "@/components/Stats.vue";
+import { mapState, mapMutations, mapActions } from "vuex";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      newLink: ""
+    };
+  },
+  components: {
+    Stats
+  },
+  computed: {
+    ...mapState(["title", "links"])
+  },
+  methods: {
+    ...mapMutations(["ADD_LINK"]),
+    ...mapActions(["removeLink"]),
+    addLink: function() {
+      this.ADD_LINK(this.newLink);
+      this.newLink = "";
+    },
+    removeLinks: function(link) {
+      this.removeLink(link);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/store.js
+++ src/store.js
@@ -1,16 +1,41 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import { resolve, timeout } from "q";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    title: "My custom Title",
+    links: ["http://facebook.com", "https://google.com"]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length;
+    }
   },
   mutations: {
-
+    ADD_LINK: (state, link) => {
+      state.links.push(link);
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1);
+    },
+    REMOVE_ALL: state => {
+      state.links = [];
+    }
   },
   actions: {
-
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link);
+    },
+    removeAll({ commit }) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit("REMOVE_ALL");
+          resolve();
+        }, 1500);
+      });
+    }
   }
-})
+});
